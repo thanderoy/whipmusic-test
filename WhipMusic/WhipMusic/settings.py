4d1b8e7b48725a35635e2795bad0dbb7dfc115e8
thanderoy <roy.thande@gmail.com>
2021-12-23 15:57:08 +0300

    Fixes to settings.py 1

--- WhipMusic/WhipMusic/settings.py
+++ WhipMusic/WhipMusic/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['whipmusic-test.herokuapp.com', 'https://whipmusic-test.herokuapp.com/']
 
